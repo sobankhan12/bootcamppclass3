6a31f135255afa980dafe8b38393362949cdfd51
Soban khan <sobankhan252@gmail.com>
2020-06-08 09:24:28 +0500

    Update Student.js

--- src/Student.js
+++ src/Student.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import image from './786.jpg';
+
 
 
 const Student = (props) => {
@@ -9,7 +9,7 @@ const Student = (props) => {
         <h1>Hello <strong>{props.name}</strong></h1>
         <h3>sum of 102 + 23 is {props.sum}</h3>
         <p>this is my image</p>
-        <img src={image} width="200px" height="200px"  />
+        
         <div>
             <h3>Fruits</h3>
             <ul>
